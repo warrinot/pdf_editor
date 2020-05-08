02f7545e6af94d5540c3e8cddd334ad8634cdf84
Warrinot <warrinot@gmail.com>
2020-05-08 17:11:43 +0300

    added feature to show num pages of selected files and opening target folder after split

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import PySimpleGUI as sg
 import re
 import PyPDF2
 import os
+import subprocess
 
 
-def find_pages(string_of_pages: list()):
-    if '-' in string_of_pages:
+def find_pages(string_of_pages: list()):  # TODO: Доработать.
+    if '-' in string_of_pages and ',' not in string_of_pages:
         page_nums = string_of_pages.split('-')
         page_nums = list(range(int(page_nums[0]) - 1, int(page_nums[1])))
+
+    # elif '-' in string_of_pages and ',' in string_of_pages:
+        # exp = r'\d+-\d+'
 
     else:
         page_nums = re.findall(r'\d+', string_of_pages)
@@ -53,9 +57,10 @@ def split_doc(filename: str, pages_to_split: list, new_name: str, onefile: bool)
             new_file.close()
 
     pdf_file.close()
+    return folder
 
 
-def merge_docs(filenames, saveas):
+def merge_docs(filenames: list, saveas: str):
     pdf_merger = PyPDF2.PdfFileMerger()
     for file in filenames:
         pdf_file = open(file, 'rb')
@@ -71,10 +76,10 @@ def merge_docs(filenames, saveas):
 def main():
     sg.theme('Dark Blue 3')
     layout = [[sg.Text('Выберите действие:')],
-              [sg.Button('Split')],
-              [sg.Button('Merge', disabled=False)]
+              [sg.Text('Разделить файлы - '), sg.Button('Split')],
+              [sg.Text('Объединить файлы - '), sg.Button('Merge', disabled=False)]
               ]
-    window = sg.Window('Pdf things', layout, return_keyboard_events=True)
+    window = sg.Window('Pdf things', layout, return_keyboard_events=True, use_default_focus=False)
 
     while True:
         event, values = window.read()
@@ -98,8 +103,10 @@ def main():
 def split_window():
     layout_split = [[sg.Text('Выберите pdf файл, который нужно разделить')],
 
-                    [sg.In(disabled=True), sg.FileBrowse(file_types=(("Pdf-file", "*.pdf"),),
-                                                         key='file_to_split')],
+                    [sg.In(disabled=True, key='-CHOOSENFILE-'),
+                     sg.FileBrowse(file_types=(("Pdf-file", "*.pdf"),), key='-FILE-'),
+                     sg.Button('Очистить', key='-CLEAR-')],
+                    [sg.Text('', size=(30, 1), key='-NUMPAGES-')],
 
                     [sg.Text('Введите номера страниц, которые нужно отделить')],
 
@@ -108,37 +115,63 @@ def split_window():
                                        'чтобы разделить весь документ\n'))],
 
                     [sg.Radio('Сохранить страницы отдельно', "RADIO1", default=True, key='radio1'),
-                     sg.Radio('Сохранить одним файлом', "RADIO1", key='radio2')],
+                     sg.Radio('Сохранить одним файлом', "RADIO1", key='radio2', visible=False)],
 
-                    [sg.Text('Имя нового файла'), sg.Input(disabled=False, key='new_name')],
+                    [sg.Text('Имя нового файла(необязательно)'),
+                     sg.Input(disabled=False, key='new_name')],
 
                     [sg.Button('Split'), sg.Exit()]]
 
     window_split = sg.Window('Pdf Split', layout_split, return_keyboard_events=True)
     window_split.Finalize()
     window_split.TKroot.focus_force()  # make active
-
+    num_pages_shown = False
     while True:
-        event, values = window_split.read(timeout=100)
+        event, values = window_split.read(timeout=1000)
+        # print(event, values)
 
         if event is None or event == 'Exit' or event == 'Escape:27':
             break
 
-        elif event == '-IN-' and values['-IN-'] and values['-IN-'][-1] not in ('0123456789, -'):
+        if values['-IN-']:
+            window_split['radio2'].Update(visible=True)
+        else:
+            window_split['radio2'].Update(visible=False)
+
+        choosenfile = values['-CHOOSENFILE-']
+
+        if not num_pages_shown and choosenfile:
+            with open(choosenfile, 'rb') as opened_file:
+                pdf_read_now = PyPDF2.PdfFileReader(opened_file, strict=False)
+                window_split['-NUMPAGES-'].update(
+                    f'Страниц в выбранном файле: {pdf_read_now.numPages}')
+                num_pages_shown = True
+
+        if event == '-IN-' and values['-IN-'] and values['-IN-'][-1] not in ('0123456789, -'):
             window_split['-IN-'].update(values['-IN-'][:-1])
 
         elif event == 'Split':
 
-            file_to_split = values['file_to_split']
+            file_to_split = choosenfile
             pages_to_split = find_pages(values['-IN-'])
             new_file_name = values['new_name']
             onefile = values['radio2']
 
             try:
-                split_doc(file_to_split, pages_to_split, new_file_name, onefile)
+                folder = split_doc(file_to_split, pages_to_split, new_file_name, onefile)
+                # sg.popup_ok(f'Сохранено в папке {folder}')
+                path_to_open = os.path.normpath(folder)
+                print(path_to_open)
+                subprocess.Popen(f'explorer "{path_to_open}"')
+
             except FileNotFoundError:
                 sg.popup_ok('Не выбран файл.')
 
+        elif event == '-CLEAR-':
+            window_split['-CHOOSENFILE-'].Update('')
+            window_split['-NUMPAGES-'].Update('')
+            window_split['-FILE-'].values = ''
+            num_pages_shown = False
     window_split.close()
 
 
@@ -148,6 +181,8 @@ def merge_window():
 
         [sg.In(key='-FILE_INPUT-', disabled=True, enable_events=True),
          sg.FilesBrowse(file_types=(("Pdf-file", "*.pdf"),))],
+        # [sg.Text('Выбраны файлы:')],
+        # [sg.Multiline('', size=(50, 10), key='-FILESMERGE-', auto_size_text=True, disabled=True)],
 
         [sg.Text('Сохранить как:')],
 
@@ -156,10 +191,6 @@ def merge_window():
 
         [sg.Button('Merge'), sg.Exit()],
 
-
-
-
-
     ]
 
     window_merge = sg.Window('Pdf merge', layout_merge, return_keyboard_events=True)
@@ -167,12 +198,14 @@ def merge_window():
     window_merge.TKroot.focus_force()  # make active
 
     while True:
-        event, values = window_merge.read()
+        event, values = window_merge.read(timeout=100)
+
         if event is None or event == 'Exit' or event == 'Escape:27':
             break
 
         elif event == '-SAVEASIN-':
             saveas_value = values['-SAVEASIN-']
+
             if not saveas_value.endswith('.pdf'):
                 saveas_value += '.pdf'
             window_merge['-SAVEASIN-'].Update(saveas_value)
@@ -193,6 +226,7 @@ def merge_window():
 
             else:
                 merge_docs(input_files, saveas_value)
+                sg.popup_ok(f'Файлы объединены в {saveas_value}')
 
     window_merge.close()
 
